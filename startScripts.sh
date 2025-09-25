1becbff4403085c1735dab6e00a8e6b3e356c231
adamjmilward@gmail.com <adamjmilward@gmail.com>
2025-09-25 21:13:04 +0100

    update start script

--- startScripts.sh
+++ startScripts.sh
@@ -1,22 +1,9 @@
-screen -dmS watch bash -c 'python /home/adam/python/watchdog.py' &&
-screen -dmS heat0 bash -c 'python /home/adam/python/incubate.py' &&
+screen -dmS watch bash -c 'python /home/adam/webBarron/watchdog.py' &&
+sleep 30 &&
+screen -dmS heat1 bash -c 'python /home/adam/webBarron/pidIncubate.py' &&
 sleep 60 &&
-screen -dmS heat1 bash -c 'python /home/adam/python/incubate.py' &&
-sleep 30 &&
-screen -dmS heat2 bash -c 'python /home/adam/python/incubate.py' &&
-sleep 30 &&
-screen -dmS heat3 bash -c 'python /home/adam/python/incubate.py' &&
-sleep 30 &&
-screen -dmS heat4 bash -c 'python /home/adam/python/incubate.py' &&
-sleep 30 &&
-screen -dmS heat5 bash -c 'python /home/adam/python/incubate.py' &&
-sleep 30 &&
-screen -dmS heat6 bash -c 'python /home/adam/python/incubate.py' &&
-sleep 30 &&
-screen -dmS heat7 bash -c 'python /home/adam/python/incubate.py' &&
-sleep 30 &&
-screen -dmS heat8 bash -c 'python /home/adam/python/incubate.py' &&
-sleep 30 &&
-screen -dmS heat9 bash -c 'python /home/adam/python/incubate.py' &&
+screen -dmS heat2 bash -c 'python /home/adam/webBarron/pidIncubate.py' &&
+sleep 60 &&
+screen -dmS heat3 bash -c 'python /home/adam/webBarron/pidIncubate.py' &&
 sleep 5 &&
 screen -dmS finished bash -c 'ping 192.168.4.0'
